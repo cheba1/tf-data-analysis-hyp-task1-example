636a2d64d70abbb60afbf71e791c51398551d884
cheba1 <113043236+cheba1@users.noreply.github.com>
2023-04-07 13:07:55 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,4 +11,13 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=0.02
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
+    z_value = (p1 - p2) / sqrt(p_combined * (1 - p_combined) * (1 / x_cnt + 1 / y_cnt))
+    if abs(z_value) > norm.ppf(1 - alpha / 2):
+        return True  # отвергаем H0
+    else:
+        return False  # не отвергаем H0
+    
